e5c248c882d3263e9e27f3d657d4922010a8d450
KostinKD <omroners@gmail.com>
2021-01-27 20:50:14 +0300

    main

--- js/preloader.js
+++ js/preloader.js
@@ -5,13 +5,13 @@ paceOptions = {
 };
 
 Pace.on('done', function () {
-    $(".p").delay(500).animate({ top: '300', opacity: '0' }, 3000, $.bez([0.19, 1, 0.22, 1]));
-    $(".pace").delay(500).animate({opacity: '0' }, 2000, $.bez([0.19, 1, 0.22, 1]));
+    $(".p").delay(500).animate({ top: '300', opacity: '0' }, 1000, $.bez([0.19, 1, 0.22, 1]));
+    $(".pace").delay(500).animate({opacity: '0' }, 1000, $.bez([0.19, 1, 0.22, 1]));
 
-    $(".preloader").delay(1500).animate({ top: '-1000' }, 2000, $.bez([0.19, 1, 0.22, 1]));
+    $(".preloader").delay(1500).animate({ top: '-1000' }, 1000, $.bez([0.19, 1, 0.22, 1]));
 
     TweenMax.from(".wrapper", 2, {
-        delay: 1.8,
+        delay: 1,
         y: 10,
         opacity: 0,
         ease: Expo.easeInOut
